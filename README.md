fe626a97e04a96e8a413f4a7c123741ec8d51191
Greg <cbuctok@gmail.com>
2015-09-29 11:16:32 +0200

    init
    First commit. Looking good!

---
+++ README.md
@@ -0,0 +1,12 @@
+## Stop Win10 Telemetry
+
+* this script blocks telemetry via firewall, at least tries to
+* also removes shady schedules
+* you can block hosts file by yourself, but it's a great possibility that it won't work
+* based on http://habrahabr.ru/post/267507/
+
+**What do**
+
+1. Run main.bat via console as Administrator
+2. Press Enter when asked, you can read if you want, but you can't answer so why bother?
+3. Placebo engaged!
---
+++ main.bat
@@ -0,0 +1,86 @@
+@echo off
+cls
+echo Telemetry 
+echo Rules of Firewall
+echo.
+echo press any key to continue...
+pause > NUL
+echo Rules of Firewall 
+echo.
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File ""%~dp0.\blocklist.ps1""' -Verb RunAs}"
+echo Rules included in Firewall...
+echo.
+echo DELETE DiagTrack and dmwappushservice?
+pause > NUL
+sc delete DiagTrack
+sc delete dmwappushservice
+echo Stop schedules?
+pause > NUL
+REM *** Task that collects data for SmartScreen in Windows ***
+schtasks /Change /TN "Microsoft\Windows\AppID\SmartScreenSpecific" /Disable
+
+REM *** Collects program telemetry information if opted-in to the Microsoft Customer Experience Improvement Program ***
+schtasks /Change /TN "Microsoft\Windows\Application Experience\ProgramDataUpdater" /Disable
+
+REM *** Collects program telemetry information if opted-in to the Microsoft Customer Experience Improvement Program ***
+schtasks /Change /TN "Microsoft\Windows\Application Experience\Microsoft Compatibility Appraiser" /Disable
+
+REM *** Aggregates and uploads Application Telemetry information if opted-in to the Microsoft Customer Experience Improvement Program ***
+schtasks /Change /TN "Microsoft\Windows\Application Experience\AitAgent" /Disable
+
+REM *** This task collects and uploads autochk SQM data if opted-in to the Microsoft Customer Experience Improvement Program ***
+schtasks /Change /TN "Microsoft\Windows\Autochk\Proxy" /Disable
+
+REM *** If the user has consented to participate in the Windows Customer Experience Improvement Program, this job collects and sends usage data to Microsoft ***
+schtasks /Change /TN "Microsoft\Windows\Customer Experience Improvement Program\Consolidator" /Disable
+
+REM *** The Kernel CEIP (Customer Experience Improvement Program) task collects additional information about the system and sends this data to Microsoft. *** 
+REM *** If the user has not consented to participate in Windows CEIP, this task does nothing ***
+schtasks /Change /TN "Microsoft\Windows\Customer Experience Improvement Program\KernelCeipTask" /Disable
+
+REM *** The Bluetooth CEIP (Customer Experience Improvement Program) task collects Bluetooth related statistics and information about your machine and sends it to Microsoft ***
+REM *** The information received is used to help improve the reliability, stability, and overall functionality of Bluetooth in Windows ***
+REM *** If the user has not consented to participate in Windows CEIP, this task does not do anything.***
+schtasks /Change /TN "Microsoft\Windows\Customer Experience Improvement Program\BthSQM" /Disable
+
+REM *** Create Object Task ***
+schtasks /Change /TN "Microsoft\Windows\CloudExperienceHost\CreateObjectTask" /Disable
+
+REM *** The Windows Disk Diagnostic reports general disk and system information to Microsoft for users participating in the Customer Experience Program ***
+schtasks /Change /TN "Microsoft\Windows\DiskDiagnostic\Microsoft-Windows-DiskDiagnosticDataCollector" /Disable
+
+REM *** Measures a system's performance and capabilities ***
+schtasks /Change /TN "Microsoft\Windows\Maintenance\WinSAT" /Disable
+
+REM *** Network information collector ***
+schtasks /Change /TN "Microsoft\Windows\NetTrace\GatherNetworkInfo" /Disable
+
+REM *** Initializes Family Safety monitoring and enforcement ***
+schtasks /Change /TN "Microsoft\Windows\Shell\FamilySafetyMonitor" /Disable
+
+REM *** Synchronizes the latest settings with the Family Safety website ***
+schtasks /Change /TN "Microsoft\Windows\Shell\FamilySafetyRefresh" /Disable
+
+REM *** SQM (Software Quality Management) ***
+schtasks /Change /TN "Microsoft\Windows\IME\SQM data sender" /Disable
+
+REM *** This task initiates the background task for Office Telemetry Agent, which scans and uploads usage and error information for Office solutions ***
+schtasks /Change /TN "Microsoft\Office\OfficeTelemetryAgentFallBack" /Disable
+
+REM *** This task initiates Office Telemetry Agent, which scans and uploads usage and error information for Office solutions when a user logs on to the computer ***
+schtasks /Change /TN "Microsoft\Office\OfficeTelemetryAgentLogOn" /Disable
+
+echo Also experimental?
+pause > NUL
+EM *** Scans startup entries and raises notification to the user if there are too many startup entries ***
+schtasks /Change /TN "Microsoft\Windows\Application Experience\StartupAppTask" /Disable
+
+REM *** Protects user files from accidental loss by copying them to a backup location when the system is unattended ***
+schtasks /Change /TN "Microsoft\Windows\FileHistory\File History (maintenance mode)" /Disable
+
+REM *** This task gathers information about the Trusted Platform Module (TPM), Secure Boot, and Measured Boot ***
+schtasks /Change /TN "Microsoft\Windows\PI\Sqm-Tasks" /Disable
+
+REM *** This task analyzes the system looking for conditions that may cause high energy use ***
+schtasks /Change /TN "Microsoft\Windows\Power Efficiency Diagnostics\AnalyzeSystem" /Disable
+pause
